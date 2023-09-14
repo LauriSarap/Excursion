2c16dc63bd272003d9c30cc7bbe95022e2bd9425
Lauri Sarap <lauri.aardla@gmail.com>
2023-09-14 09:39:41 +0300

    updates

--- ExcursionLibrary/src/Core/FCLayer.h
+++ ExcursionLibrary/src/Core/FCLayer.h
@@ -7,7 +7,4 @@ public:
     FCLayer(int input_size, int output_size);
     virtual Eigen::MatrixXd forward_propagation(const Eigen::MatrixXd& input_data) override;
     virtual Eigen::MatrixXd backward_propagation(const Eigen::MatrixXd& output_error_data, double learning_rate) override;
-    
-    //Eigen::MatrixXd weights;
-    //Eigen::MatrixXd biases;
 };
