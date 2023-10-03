f6a31a8cdfa0ec94e3429a9133a2e98b7967d2ae
Lauri Sarap <lauri.aardla@gmail.com>
2023-10-03 15:03:26 +0300

    New activation functions

--- ExcursionLibrary/src/Core/ActivationFunctions.h
+++ ExcursionLibrary/src/Core/ActivationFunctions.h
@@ -15,3 +15,41 @@ inline Eigen::MatrixXd tanh_derivative(const Eigen::MatrixXd& x)
     // tanh_function' = 1 - tanh_function^2
     return x.unaryExpr([](double val) { return 1 - std::pow(std::tanh(val), 2); });
 }
+
+inline Eigen::MatrixXd sigmoid_function(const Eigen::MatrixXd& x)
+{
+    // Applies the sigmoid function element-wise to the input matrix using Eigen's unaryExpr method with a lambda expression.
+    // sigmoid_function = 1 / (1 + e^-x)
+    return x.unaryExpr([](double val) { return 1 / (1 + std::exp(-val)); });
+}
+
+inline Eigen::MatrixXd sigmoid_derivative(const Eigen::MatrixXd& x)
+{
+    return x.unaryExpr([](double val)
+    {
+        double sigmoid = 1 / (1 + std::exp(-val));
+        return sigmoid * (1 - sigmoid);
+    });
+}
+
+inline Eigen::MatrixXd relu_function(const Eigen::MatrixXd& x)
+{
+    return x.unaryExpr([](double val) { return std::max(0.0, val); });
+}
+
+inline Eigen::MatrixXd relu_derivative(const Eigen::MatrixXd& x)
+{
+    return x.unaryExpr([](double val) { return val > 0.0 ? 1.0 : 0.0; });
+}
+
+inline Eigen::MatrixXd leaky_relu_function(const Eigen::MatrixXd& x)
+{
+    return x.unaryExpr([](double val) { return val >= 0 ? val : 0.01 * val; });
+}
+
+inline Eigen::MatrixXd leaky_relu_derivative(const Eigen::MatrixXd& x)
+{
+    return x.unaryExpr([](double val) { return val >= 0 ? 1.0 : 0.01; });
+}
+
+
